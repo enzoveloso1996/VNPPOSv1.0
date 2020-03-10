c29fce5503c2758db79f3fab7ac13d40122528b4
enzoveloso1996 <enzo.veloso888@gmail.com>
2020-03-11 01:30:06 +0800

    up

--- VNPPOS/VNPPOS/Bootstrapper.cs
+++ VNPPOS/VNPPOS/Bootstrapper.cs
@@ -18,7 +18,7 @@ namespace VNPPOS
 
         protected override void OnStartup(object sender, StartupEventArgs e)
         {
-            DisplayRootViewFor<PosViewModel>();
+            DisplayRootViewFor<LoginViewModel>();
         }
     }
 }
