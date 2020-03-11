6d19bd21646a9b94d5c62ef9bde8783773ce2a60
enzoveloso1996 <enzo.veloso888@gmail.com>
2020-03-11 22:52:58 +0800

    up

--- VNPPOS/VNPPOS/Views/PosView.xaml.cs
+++ VNPPOS/VNPPOS/Views/PosView.xaml.cs
@@ -23,5 +23,87 @@ namespace VNPPOS.Views
         {
             InitializeComponent();
         }
+
+        private void shirtTab(object sender, RoutedEventArgs e)
+        {
+            shirts.Visibility = Visibility.Visible;
+            jackets.Visibility = Visibility.Collapsed;
+            pants.Visibility = Visibility.Collapsed;
+            shorts.Visibility = Visibility.Collapsed;
+            bags.Visibility = Visibility.Collapsed;
+            caps.Visibility = Visibility.Collapsed;
+            polo.Visibility = Visibility.Collapsed;
+        }
+
+        private void jacketTab(object sender, RoutedEventArgs e)
+        {
+            shirts.Visibility = Visibility.Collapsed;
+            jackets.Visibility = Visibility.Visible;
+            pants.Visibility = Visibility.Collapsed;
+            shorts.Visibility = Visibility.Collapsed;
+            bags.Visibility = Visibility.Collapsed;
+            caps.Visibility = Visibility.Collapsed;
+            polo.Visibility = Visibility.Collapsed;
+        }
+
+        private void poloTab(object sender, RoutedEventArgs e)
+        {
+            shirts.Visibility = Visibility.Collapsed;
+            jackets.Visibility = Visibility.Collapsed;
+            pants.Visibility = Visibility.Collapsed;
+            shorts.Visibility = Visibility.Collapsed;
+            bags.Visibility = Visibility.Collapsed;
+            caps.Visibility = Visibility.Collapsed;
+            polo.Visibility = Visibility.Visible;
+        }
+
+        private void pantsTab(object sender, RoutedEventArgs e)
+        {
+            shirts.Visibility = Visibility.Collapsed;
+            jackets.Visibility = Visibility.Collapsed;
+            pants.Visibility = Visibility.Visible;
+            shorts.Visibility = Visibility.Collapsed;
+            bags.Visibility = Visibility.Collapsed;
+            caps.Visibility = Visibility.Collapsed;
+            polo.Visibility = Visibility.Collapsed;
+        }
+
+        private void shortTab(object sender, RoutedEventArgs e)
+        {
+            shirts.Visibility = Visibility.Collapsed;
+            jackets.Visibility = Visibility.Collapsed;
+            pants.Visibility = Visibility.Collapsed;
+            shorts.Visibility = Visibility.Visible;
+            bags.Visibility = Visibility.Collapsed;
+            caps.Visibility = Visibility.Collapsed;
+            polo.Visibility = Visibility.Collapsed;
+        }
+
+        private void bagTab(object sender, RoutedEventArgs e)
+        {
+            shirts.Visibility = Visibility.Collapsed;
+            jackets.Visibility = Visibility.Collapsed;
+            pants.Visibility = Visibility.Collapsed;
+            shorts.Visibility = Visibility.Collapsed;
+            bags.Visibility = Visibility.Visible;
+            caps.Visibility = Visibility.Collapsed;
+            polo.Visibility = Visibility.Collapsed;
+        }
+
+        private void capsTab(object sender, RoutedEventArgs e)
+        {
+            shirts.Visibility = Visibility.Collapsed;
+            jackets.Visibility = Visibility.Collapsed;
+            pants.Visibility = Visibility.Collapsed;
+            shorts.Visibility = Visibility.Collapsed;
+            bags.Visibility = Visibility.Collapsed;
+            caps.Visibility = Visibility.Visible;
+            polo.Visibility = Visibility.Collapsed;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            dateTime.Text = DateTime.Now.ToString();
+        }
     }
 }
